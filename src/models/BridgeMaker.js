0e03854d1fc660036557884c565e6342e654b86a
suhwa <tnghk9611@naver.com>
2024-12-12 16:16:17 +0900

    feat: 다리 생성하는 기능 구현

--- src/models/BridgeMaker.js
+++ src/models/BridgeMaker.js
@@ -1,5 +1,14 @@
 const BridgeMaker = {
-  makeBridge(size, generateRandomNumber) {},
+  // size만큼 다리 생성해서 0/1 채운다음 반환
+  makeBridge(size, generateRandomNumber) {
+    const bridge = Array.from({ length: size }, () => 0);
+
+    for (let s = 0; s < size; s++) {
+      bridge[s] = generateRandomNumber.generate();
+    }
+
+    return bridge;
+  },
 };
 
 export default BridgeMaker;

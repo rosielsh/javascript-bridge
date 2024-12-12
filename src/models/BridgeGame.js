a9da460b7c0fd09e40ae0204f655aedee1161558
suhwa <tnghk9611@naver.com>
2024-12-12 16:34:07 +0900

    feat: 사용자의 입력에 따라 움직이는 기능 구현

--- src/models/BridgeGame.js
+++ src/models/BridgeGame.js
@@ -1,7 +1,37 @@
 class BridgeGame {
-  move() {}
+  #bridge;
+  #step; // 현재까지 맞은 개수(다음 위치)
 
-  retry() {}
+  constructor(bridge) {
+    this.#bridge = bridge;
+    this.#step = 0;
+  }
+
+  // 갈 수 있는지의 여부를 확인하고 반환
+  move(direction) {
+    if (direction === "U") {
+      if (this.#bridge[this.#step] === 1) {
+        this.#step++;
+        return true;
+      }
+      return false;
+    }
+
+    if (this.#bridge[this.#step++] === 0) {
+      this.#step++;
+      return true;
+    }
+
+    return false;
+  }
+
+  retry() {
+    this.#step = 0;
+  }
+
+  isFinish() {
+    return this.#bridge.length === this.#step;
+  }
 }
 
 export default BridgeGame;

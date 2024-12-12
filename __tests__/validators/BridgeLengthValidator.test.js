c60bc9003f87862eacd587b984913ae7a4b1f365
suhwa <tnghk9611@naver.com>
2024-12-12 15:50:31 +0900

    test: 다리 길이 예외 테스트 작성

---
+++ __tests__/validators/BridgeLengthValidator.test.js
@@ -0,0 +1,17 @@
+describe("BridgeLengthValidator 클래스 테스트", () => {
+  test("다리 길이는 숫자가 아니면 에러가 발생한다", () => {
+    const lengths = [undefined, null, NaN, {}, [], "string"];
+
+    lengths.forEach((length) => {
+      expect(() => BridgeLengthValidator.validate(length)).toThrow();
+    });
+  });
+
+  test("다리 길이는 3~20사이의 숫자가 아니면 에러가 발생한다", () => {
+    const lengths = [0, 21, Infinity];
+
+    lengths.forEach((length) => {
+      expect(() => BridgeLengthValidator.validate(length)).toThrow();
+    });
+  });
+});
